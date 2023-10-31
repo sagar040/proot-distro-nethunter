e6e87b9131643ef59e92d9261dbdf20fcea7ac4c
sagar040 <001sagarbiswas@gmail.com>
2023-10-31 22:27:31 +0530

    Version 1.5

--- install-nethunter.sh
+++ install-nethunter.sh
@@ -20,7 +20,7 @@
 
 set -e
 
-SCRIPT_VERSION="1.4"
+SCRIPT_VERSION="1.5"
 
 banner() {
     clear
@@ -39,6 +39,8 @@ banner() {
     local C11='\e[38;5;127m'
     local R='\033[0m'
     local S=$(printf '%*s' 12 '')
+    local S2=$(printf '%*s' 8 '')
+    local S3=$(printf '%*s' 15 '')
     echo -e "$B$S$C1......., ,,.."
     echo -e "$S        ...cnx,"
     echo -e "$S$C2 $C5..$R$C2.'''$B$C2 ...:lb;."
@@ -55,17 +57,15 @@ banner() {
     echo -e "$S$C10                      b $C10 '.$C3"
     echo -e "$S$C11                       c"
     echo -e "$S                       '\n"
+    echo -e "$S$S2\e[38;5;231;1mPRoo\e[38;5;253;1mt D\e[38;5;251;1mis\e[38;5;248;1mtro \e[38;5;196;1mNet\e[38;5;160;1mhun\e[38;5;124;1mter\e[0m$S3\e[38;5;155;4mv$SCRIPT_VERSION\033[0m"
 }
-
 # rootfs path
 nh_rootfs="$PREFIX/var/lib/proot-distro/installed-rootfs/BackTrack-linux"
 
 # info
-info() {
+info(){
     banner
-    echo -e "\e[38;5;242;1m  -------------- Kali Nethunter Installer --------------\033[0m\n"
-    echo -e "\e[38;5;253mInstall Kali Nethunter (official version) on proot-distro. This is a Bash script that automates the installation of Kali NetHunter with the proot-distro tool.\033[0m\n"
-    echo -e "\e[38;5;45mVersion:\033[0m \033[1;33m$SCRIPT_VERSION\033[0m\n"
+    echo -e "\n\e[38;5;250mInstall Kali Nethunter (official version) on proot-distro. This is a Bash script that automates the installation of Kali NetHunter with the proot-distro tool.\033[0m\n"
     echo -e "\e[38;5;45mUsage:\033[0m $0 --install\n"
     echo -e "\e[38;5;45mGithub Repo:\033[0m \e[38;5;240mhttps://github.com/sagar040/proot-distro-nethunter\033[0m\n"
     echo -e ""
@@ -108,7 +108,8 @@ select_installation_type() {
     echo -e "NO     installation type          required disk space\n"
     echo -e "1.     nethunter                       6 GB"
     echo -e "2.     nethunter lite                  4 GB"
-    echo -e "3.     nethunter terminal (no GUI)   2.5 GB\n"
+    echo -e "3.     nethunter terminal (no GUI)   2.5 GB"
+    echo -e "4.     nethunter  (blank)            1.3 GB\n"
 
     read -p "Select installation type (default: 1): " installation_type
 
@@ -116,6 +117,7 @@ select_installation_type() {
         1) img="default";;
         2) img="lite";;
         3) img="ngui";;
+        4) img="blank";;
         *) img="default";;
     esac
 }
@@ -162,6 +164,10 @@ pkgs_nh_terminal(){
     proot-distro login BackTrack-linux -- apt install -y nmap ncat python3 python3-pip socat john hydra  gobuster dirb dirbuster wordlists vim binwalk cewl wpscan ffuf wfuzz sqlmap sqlite3 nikto villain autopsy termshark
 }
 
+nh_blank(){
+    proot-distro login BackTrack-linux -- apt autoremove -y
+}
+
 # Setup Nethunter
 setup_nethunter(){
     # hide Kali developers message
@@ -171,8 +177,64 @@ setup_nethunter(){
     proot-distro login BackTrack-linux -- bash -c 'apt update
     apt upgrade -y
     apt autoremove -y
-    apt install apt-utils command-not-found
+    apt install -y apt-utils command-not-found
     echo "kali    ALL=(ALL:ALL) ALL" > /etc/sudoers.d/kali'
+}
+
+# Update default password
+update_passwd(){
+    printf "\n\033[33;1minstaller:\033[0m Update password (root)\n"
+    proot-distro login BackTrack-linux -- passwd
+    printf "\n\033[33;1minstaller:\033[0m Update password (kali)\n"
+    proot-distro login BackTrack-linux -- passwd kali
+}
+
+# Setup zsh prompt (ohmyzsh)
+setup_zsh(){
+    printf "\033[33;1minstaller:\033[0m"
+    read -p " Apply nethunter theme to Termux? (y/n): " snt
+    case "$snt" in
+        y|Y)
+            if [ -f "$HOME/.termux/colors.properties" ]; then
+                mv "$HOME/.termux/colors.properties" "$HOME/.termux/colors.properties-$(date +%d.%m.%y-%H:%M:%S).old"
+            fi
+            cp "./nethunter-theme/termux/colors.properties" "$HOME/.termux"
+            cp "./nethunter-theme/termux/font.ttf" "$HOME/.termux"
+            ;;
+        *)
+            # Do nothing
+            ;;
+    esac
+    
+    proot-distro login BackTrack-linux -- bash -c 'git clone https://github.com/ohmyzsh/ohmyzsh.git "/opt/oh-my-zsh" --depth 1
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "/opt/zsh-syntax-highlighting" --depth 1
+    cp -r "/opt/oh-my-zsh" "/root/.oh-my-zsh"
+    cp -r "/opt/oh-my-zsh" "/home/kali/.oh-my-zsh"
+    cp -r "/opt/zsh-syntax-highlighting" "/root/.zsh-syntax-highlighting"
+    cp -r "/opt/zsh-syntax-highlighting" "/home/kali/.zsh-syntax-highlighting"
+    cp "/opt/oh-my-zsh/templates/zshrc.zsh-template" "/root/.zshrc"
+    cp "/opt/oh-my-zsh/templates/zshrc.zsh-template" "/home/kali/.zshrc"'
+    
+    cp "./nethunter-theme/kali-ohmyzsh-theme/kali.zsh-theme" "$nh_rootfs/root/.oh-my-zsh/themes"
+    cp "./nethunter-theme/kali-ohmyzsh-theme/kali.zsh-theme" "$nh_rootfs/home/kali/.oh-my-zsh/themes"
+    
+    proot-distro login BackTrack-linux -- sed -i '/^ZSH_THEME/d' "/root/.zshrc"
+    proot-distro login BackTrack-linux -- sed -i '1iZSH_THEME="kali"' "/root/.zshrc"
+    proot-distro login BackTrack-linux --user kali -- sed -i '/^ZSH_THEME/d' "/home/kali/.zshrc"
+    proot-distro login BackTrack-linux --user kali -- sed -i '1iZSH_THEME="kali"' "/home/kali/.zshrc"
+    
+    proot-distro login BackTrack-linux -- bash -c 'echo "source /root/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "/root/.zshrc"
+    echo "source /etc/zsh_command_not_found" >> /root/.zshrc
+    rm -rf /opt/oh-my-zsh
+    rm -rf /opt/zsh-syntax-highlighting'
+    proot-distro login BackTrack-linux --user kali -- bash -c 'echo "source /home/kali/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "/home/kali/.zshrc"
+    echo "source /etc/zsh_command_not_found" >> /home/kali/.zshrc'
+    
+    # change it from sufficient to required after this operation
+    proot-distro login BackTrack-linux -- sed -i '8c\auth       sufficient   pam_shells.so' "/etc/pam.d/chsh"
+    
+    proot-distro login BackTrack-linux -- chsh -s /usr/bin/zsh
+    proot-distro login BackTrack-linux --user kali -- chsh -s /usr/bin/zsh
 }
 
 # Set up Nethunter GUI
@@ -199,13 +261,19 @@ if [[ $1 == "--install" ]]; then
     generate_config_file
     
     printf "\n\033[33;1minstaller:\033[0m Distribution added as BackTrack-linux\n"
+    sleep 2
     
     # Install Nethunter
     proot-distro install BackTrack-linux
     
     # Update and setup
     printf "\n\033[33;1minstaller:\033[0m Setting up nethunter...\n"
+    sleep 3
     setup_nethunter
+    update_passwd
+    printf "\n\033[33;1minstaller:\033[0m Setting zsh prompt...\n"
+    sleep 4
+    setup_zsh
     
     # installation function based on the installation_type
     if [ "$installation_type" = "default" ]; then
@@ -216,9 +284,11 @@ if [[ $1 == "--install" ]]; then
         gui_setup
     elif [ "$installation_type" = "ngui" ]; then
         pkgs_nh_terminal
+    elif [ "$installation_type" = "blank" ]; then
+        nh_blank
     fi
     
-    echo -e "\n\033[33;1minstaller:\033[0m successfully installed nethunter\n"
+    echo -e "\n\033[33;1minstaller:\033[0m \033[32;1mSuccessfully installed nethunter\033[0m\n"
     
     # Shortcut
     echo '[ -n "$1" ] && proot-distro login BackTrack-linux --user "$1" || proot-distro login BackTrack-linux' > $PREFIX/bin/nethunter
@@ -227,6 +297,7 @@ if [[ $1 == "--install" ]]; then
     # Print instructions
     echo -e "\nLogin: \033[32mnethunter [user]\033[0m (default=root)"
     echo -e "\n\033[33;1minstaller:\033[0m For GUI access, run \033[32mkgui\033[0m command. (after login into nethunter)"
+    echo -e "\n\033[33;1minstaller:\033[0m \033[34;1mPlease restart the termux app\033[0m"
 else
     info
 fi
